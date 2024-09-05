d0baab7c58dd6f585875a0baacbe0585ccb45826
nevita <triyanevita7@gmail.com>
2024-09-05 17:18:15 +0700

    perkenalan nevita

---
+++ perkenalan22.java
@@ -0,0 +1,15 @@
+/**
+ *Perkenalan 22
+ */
+public class Perkenalan22 {
+    public static void main (String [] args) {
+        System.out.println("-".repeat(count:56));
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINAMA --");
+        System.out.println("-".repeat(count:56));
+        System.out.println("Nama :  Nevita Triya Yuliana");
+        System.out.println("NIM : 244107020208 \nKelas : 1D \nNoAbsen : 22");
+        System.out.println("Alamat : Jl. Bunga Merak 2 Kav16, Malang");
+        System.out.println("Nomor telepon : 089697240083");
+        System.out,println("Hobi : Futsal");
+    }
+}

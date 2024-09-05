549c287e2bb5cc15219da19aafe6db1308a502f5
nevita <triyanevita7@gmail.com>
2024-09-05 16:52:22 +0700

    perkenalan jasmine

---
+++ Perkenalan13.java
@@ -0,0 +1,13 @@
+/**
+ * Perkenalan 13 (ngwenchana)
+ */
+public class Perkenalan13 {
+    public static void main (String [] args) {
+        System.out.println("-".repeat(count:56));
+        System.out.println("-- Data Mahasiswa jurusan teknologi informasi POLINEMA--");
+        System.out.println("-".re[eat(count:56)])
+        System.out.println("Nama : Jasmine Nasywa Nabilah \nNIM : 244107020119 \nKelas : 1D \nNoAbsen : 13");
+        System.out.println("Alamat : Jl. Soekarno Hatta 15D, Malang.");
+        System.out.println("Nomor telepon : 085150682646 \nHobi : Menulis, Menggambar, Membaca")
+    }
+}
